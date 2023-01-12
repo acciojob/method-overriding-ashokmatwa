c1dd039323845ac568784caf4cc859556db54fdd
Ashok Matwa <ashokmatwa003@gmail.com>
2023-01-12 23:29:22 +0530

    check sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,19 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B object = new B(); //object of class B
-        object.meth();
-        //System.out.println(object.meth());
+//        object.meth();
+        System.out.println(object.meth());
     }
+}
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
 
