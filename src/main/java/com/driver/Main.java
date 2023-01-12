c5a2df9a82ba419dcbc96dba02e58e581a54d90d
Ashok Matwa <ashokmatwa003@gmail.com>
2023-01-12 23:14:25 +0530

    print

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B object = new B(); //object of class B
-        String answer = object.meth();
-        System.out.println(answer);
+        object.meth();
+        //System.out.println(object.meth());
     }
 
 }
